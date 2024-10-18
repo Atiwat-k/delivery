1d440b9ec12f1fb7bd52409c339bbe2500f00166
Atiwat <atiwat.kon@gmail.com>
2024-10-19 04:25:23 +0700

    up

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./api/user'); // ตรวจสอบให้แน่ใจว่าเส้นทางถูกต้อง
 
-app.use(express.json()); // สำหรับรับข้อมูล JSON
+// Middleware สำหรับรับข้อมูล JSON
+app.use(express.json()); 
+
+// กำหนดเส้นทาง API
 app.use('/api', userRoutes);
 
+// กำหนดพอร์ตสำหรับเซิร์ฟเวอร์
 const PORT = process.env.PORT || 3000;
+
+// เริ่มเซิร์ฟเวอร์
 app.listen(PORT, (err) => {
   if (err) {
     console.error('Error starting the server:', err);

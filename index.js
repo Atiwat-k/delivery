a96eca5bf3fdc0f70456fbe7e144359874884059
Atiwat <atiwat.kon@gmail.com>
2024-10-19 03:40:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./api/user'); // ตรวจสอบให้แน่ใจว่าเส้นทางถูกต้อง
+
+app.use(express.json()); // สำหรับรับข้อมูล JSON
+app.use('/api', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error('Error starting the server:', err);
+  } else {
+    console.log(`Server is running on port ${PORT}`);
+  }
+});
